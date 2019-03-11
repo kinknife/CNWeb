0f02c4f7635fa31541c7a3021809d0c7d24cbcb7
hieudm <hieudm@coccoc.vn>
2019-03-11 13:10:19 +0700

    confign for heroku

---
+++ build/precache-manifest.ca46d8e56f887906764af58b9d0e0e35.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "b07a8f4bd87a7554b1aa",
+    "url": "/static/js/main.b2b83d25.chunk.js"
+  },
+  {
+    "revision": "0c8cc6eea09567628fe5",
+    "url": "/static/js/2.ebf57686.chunk.js"
+  },
+  {
+    "revision": "b07a8f4bd87a7554b1aa",
+    "url": "/static/css/main.87a58428.chunk.css"
+  },
+  {
+    "revision": "a907926bf6f8aabe7c9f587ccd9c196c",
+    "url": "/index.html"
+  }
+];
