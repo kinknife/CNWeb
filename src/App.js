f39673c93981b06f4c47865a5247686677b07e19
taldat <hmh982212@gmail.com>
2019-04-20 15:59:02 +0700

    Front-end
    add Signin, Signup, css

--- src/App.js
+++ src/App.js
@@ -1,197 +1,31 @@
 import React, { Component } from 'react';
 import './App.css';
-import { connectionService } from './service/connection';
+import { HashRouter as Router, Route, NavLink } from 'react-router-dom';
+import AppMain from './components/AppMain.js';
+import SignIn from './components/SignIn.js';
+import SignUp from './components/SignUp.js';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
+  render(){
+    return(
+      <Router basename="/project/">
+        <div className="App">
+          <div className="AppForm">
+            <div className="PageSwitcher">
+              <NavLink to="/home" activeClassName="SwitchActive" className="Switch">Home</NavLink>
+              <NavLink to="/sign-in" activeClassName="SwitchActive" className="Switch">Sign In</NavLink>
+              <NavLink exact to="/sign-up" activeClassName="SwitchActive" className="Switch">Sign Up</NavLink>
+            </div>
 
-    this.state = {
-      peers: [],
-      peerConnections: {},
-      iceConfig: {
-        'iceServers': [
-          {
-            'url': 'stun:stun.l.google.com:19302'
-          },
-          {
-            'url': 'turn:192.158.29.39:3478?transport=udp',
-            'credential': 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-            'username': '28224511:1379330808'
-          },
-          {
-            'url': 'turn:192.158.29.39:3478?transport=tcp',
-            'credential': 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-            'username': '28224511:1379330808'
-          }
-        ]
-      },
-      stream: null,
-      currentId: null,
-      roomId: null,
-      connected: false
-    }
-  }
-
-  componentDidMount() {
-    connectionService.subcribe(this.updateApp.bind(this));
-    connectionService.getMessagehandler(this.handleMessage.bind(this));
-  }
-
-  updateApp(msg) {
-    switch (msg.type) {
-      case 'init':
-        this.setState({
-          roomId: msg.roomId,
-          currentId: msg.id,
-          connected: msg.connected
-        });
-        break;
-      case 'new':
-        this.makeOffer(msg.id);
-        break;
-      default:
-        return;
-    }
-  }
-
-  createRoom(name) {
-    let isChrome = !!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime);
-    window.getScreenId((e, sourceId,screen_constraints) => {
-      if(!isChrome) {
-        screen_constraints.audio = true;
-      }
-      navigator.getUserMedia(screen_constraints, (s) => {
-        if(isChrome) {
-          navigator.getUserMedia({audio: true}, (audioStream) => {
-            s.addTrack(audioStream.getAudioTracks()[0]);
-            this.setState({
-              stream: s
-            });
-            connectionService.createRoom(name);
-          }, (e) => {
-            console.log(e)
-          })
-        } else {
-          this.setState({
-            stream: s
-          });
-          connectionService.createRoom(name);
-        }
-      }, (e) => {
-        console.log(e);
-      });
-    })
-  }
-
-  joinRoom(name) {
-    navigator.getUserMedia({
-      video: true,
-      audio: true
-    }, (s) => {
-      this.setState({
-        stream: s
-      });
-
-      connectionService.joinRoom(name);
-    }, (e) => {
-      console.log(e);
-    })
-  }
-
-  getPeerConnection(id) {
-    let self = this
-    if (this.state.peerConnections[id]) {
-      return this.state.peerConnections[id];
-    }
-    var pc = new RTCPeerConnection(this.state.iceConfig);
-    this.state.peerConnections[id] = pc;
-    pc.addStream(this.state.stream);
-    pc.onicecandidate = function (evnt) {
-      connectionService.sendMsg({ by: self.state.currentId, to: id, ice: evnt.candidate, type: 'ice' });
-    };
-    pc.onaddstream = function (evnt) {
-      console.log('Received new stream');
-      let peers = self.state.peers
-      peers.push({
-        id: id,
-        stream: evnt.stream
-      })
-      self.setState({
-        peers: peers
-      })
-
-      let video = document.createElement('video');
-      video.srcObject = evnt.stream;
-      video.height = 270;
-      video.width = 480;
-      video.key = id;
-      video.id = id;
-      video.autoplay = true;
-      self.videosContainer.appendChild(video);
-    };
-    return pc;
-  }
-
-  makeOffer(id) {
-    let pc = this.getPeerConnection(id);
-    let self = this
-    pc.createOffer(function (sdp) {
-      pc.setLocalDescription(sdp);
-      console.log('Creating an offer for', id);
-      connectionService.sendMsg({ by: self.state.currentId, to: id, sdp: sdp, type: 'sdp-offer' });
-    }, function (e) {
-      console.log(e);
-    },
-      { mandatory: { offerToReceiveVideo: true, offerToReceiveAudio: true } });
-  }
-
-  handleMessage(data) {
-    let pc = this.getPeerConnection(data.by);
-    let self = this
-    switch (data.type) {
-      case 'sdp-offer':
-        console.log(data.sdp);
-        pc.setRemoteDescription(new RTCSessionDescription(data.sdp), function () {
-          console.log('Setting remote description by offer');
-          pc.createAnswer(function (sdp) {
-            pc.setLocalDescription(sdp);
-            console.log(sdp)
-            connectionService.sendMsg({ by: self.state.currentId, to: data.by, sdp: sdp, type: 'sdp-answer' });
-          }, function (e) {
-            console.log(e);
-          });
-        }, function (e) {
-          console.log(e);
-        });
-        break;
-      case 'sdp-answer':
-        console.log(data.sdp);
-        pc.setRemoteDescription(new RTCSessionDescription(data.sdp), function () {
-          console.log('Setting remote description by answer');
-        }, function (e) {
-          console.error(e);
-        });
-        break;
-      case 'ice':
-        if (data.ice) {
-          console.log('Adding ice candidates');
-          console.log('join');
-          pc.addIceCandidate(new RTCIceCandidate(data.ice));
-        }
-        break;
-      default:
-        break;
-    }
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <button onClick={() => { this.createRoom('abc') }}>Create Room</button>
-        <button onClick={() => { this.joinRoom('abc') }}>Join Room</button>
-        <div className="videoContainer" ref={(videoContainer) => { this.videosContainer = videoContainer }}></div>
-      </div>
+            <Route exact path="/home" component={AppMain}>
+            </Route>
+            <Route exact path="/sign-up" component={SignUp}>
+            </Route>
+            <Route path="/sign-in" component={SignIn}>
+            </Route>
+          </div>
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/components/service/connection.js
@@ -0,0 +1,50 @@
+const io = require('socket.io-client');
+
+class ConnectionService {
+    constructor() {
+        let socket
+        if(process.env.REACT_APP_ENV !== 'production') {
+            socket = io(':4200/');
+        } else {
+            socket = io('/');
+        }
+        
+        socket.on('connect', () => {
+            this.socket = socket;
+            this.socket.on('peer.connected', (data) => {
+                let id = data.id
+                this.cb({type: 'new',id})
+            })
+            this.socket.on('incomeMsg', (data) => {
+                console.log('msg', data)
+                this.handleMessage(data)
+            })
+        });
+    }
+
+    createRoom(name) {
+        this.socket.emit('init', {name: name}, (roomId, id) => {
+            this.cb({type: 'init', roomId, id, connected: true})
+        });
+    }
+
+    joinRoom(room) {
+        this.socket.emit('init', {name: room}, (roomId, id) => {
+            this.cb({type: 'init', roomId, id, connected: true})
+        });
+    }
+
+    sendMsg(msg) {
+        this.socket.emit('msg', msg);
+    }
+
+    subcribe(cb) {
+        this.cb = cb;
+    }
+
+    getMessagehandler(cb) {
+        this.handleMessage = cb;
+    }
+}
+
+export let connectionService = new ConnectionService()
